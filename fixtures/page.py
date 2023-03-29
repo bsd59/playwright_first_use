ce1670d53f33787cd1e2c7d41a9ba261ea21d2cf
Sergii Burlaka <burlaka.eng@gmail.com>
2023-03-29 18:47:20 +0200

    Opening the google.com page in the browserÂÃÂ

--- fixtures/page.py
+++ fixtures/page.py
@@ -3,17 +3,17 @@ import pytest
 import config
 from playwright.sync_api import Browser, BrowserContext, Page, sync_playwright
 
-from config.playwright import Playwright #
-from config.expectations import Expectations #
+# from config.playwright import Playwright #
+# from config.expectations import Expectations #
 
 @pytest.fixture()
 def page() -> Page:
     playwright = sync_playwright().start()
-    if Playwright.BROWSER == 'firefox':
+    if config.playwright.BROWSER == 'firefox':
         browser = get_firefox_browser(playwright)
         context = get_context(browser)
         page_data = context.new_page()
-    elif Playwright.BROWSER == 'chrome':
+    elif config.playwright.BROWSER == 'chrome':
         browser = get_chrome_browser(playwright)
         context = get_context(browser)
         page_data = context.new_page()
@@ -30,26 +30,24 @@ def page() -> Page:
 
 def get_firefox_browser(playwright) -> Browser:
     return playwright.firefox.launch(
-        headless=config.playwright.Playwright.IS_HEADLESS,
-        slow_mo=config.playwright.Playwright.SLOW_MO,
+        headless=config.playwright.IS_HEADLESS,
+        slow_mo=config.playwright.SLOW_MO,
     )
 
 
 def get_chrome_browser(playwright) -> Browser:
     return playwright.chromium.launch(
-        headless=config.playwright.Playwright.IS_HEADLESS,
-        slow_mo=config.playwright.Playwright.SLOW_MO
+        headless=config.playwright.IS_HEADLESS,
+        slow_mo=config.playwright.SLOW_MO
     )
 
 
 def get_context(browser) -> BrowserContext:
     context = browser.new_context(
-        viewport=config.playwright.Playwright.PAGE_VIEWPORT_SIZE,
-        locale=config.playwright.Playwright.LOCALE
+        viewport=config.playwright.PAGE_VIEWPORT_SIZE,
+        locale=config.playwright.LOCALE
     )
     context.set_default_timeout(
-        # timeout=config.expectations.Expectations.DEFAULT_TIMEOUT
-        timeout=Expectations.DEFAULT_TIMEOUT
-
+        timeout=config.expectations.DEFAULT_TIMEOUT
     )
     return context
